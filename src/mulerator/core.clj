48f06c3e6279d01fd37f0a9ed56bd7bbcdf4df3f
brandoshmando <brancraft@gmail.com>
2014-09-04 17:17:05 -0600

    Started on some of the dealer module

--- src/mulerator/core.clj
+++ src/mulerator/core.clj
@@ -1,12 +1,13 @@
-(ns mulerator.core (:require [mulerator.dealer] [mulerator.optimizer]) (:gen-class))
+(ns mulerator.core (:require [mulerator.dealer :refer [packed-dolls]] [mulerator.optimizer]) (:gen-class))
 
 (defn to_i [string]
   (Integer. (re-find #"\d+" string)))
 
-(defn user-input [call]
+(defn user-input-int [call]
   (println (format "%s:" call))
   to_i(read-line))
 
 (defn -main []
-  (let [max-score (user-input "What is the Mule's weight limit today?")]
+  (let [max-score (user-input-int "What is the Mule's weight limit today?")
+        dealer-dolls ()]
     (println max-score)))
--- src/mulerator/dealer.clj
+++ src/mulerator/dealer.clj
@@ -1,2 +1,11 @@
 (ns mulerator.dealer (:gen-class))
 
+(defn user-input-str [call]
+  (println ("%s:" call))
+  (read-line))
+
+(defn packed-dolls [])
+
+;   (loop [x 0]
+;     (while (x == 1)
+;       (user-input)))
