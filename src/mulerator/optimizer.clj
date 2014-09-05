248718ef427d5cb64f979f701de50489731bfc5d
brandoshmando <brancraft@gmail.com>
2014-09-05 12:34:55 -0600

    Function that returns powerset of packed dolls is in palce

--- src/mulerator/optimizer.clj
+++ src/mulerator/optimizer.clj
@@ -1,6 +1,8 @@
 (ns mulerator.optimizer (:gen-class))
 
-
+(defn powerset [dollset]
+  (let [result (reduce (fn [dollsets x] (concat dollsets (map #(conj % x) dollsets))) [#{}] dollset)]
+      (vec result)))
 
 (defn optimize-value []
   )
