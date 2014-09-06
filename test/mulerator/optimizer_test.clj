f5a12a7338c4d0fad2dd93647e950f038df34465
brandoshmando <brancraft@gmail.com>
2014-09-05 21:58:29 -0600

    Added test for combo-wieght function

--- test/mulerator/optimizer_test.clj
+++ test/mulerator/optimizer_test.clj
@@ -4,3 +4,8 @@
   (def somevar [{:doll-value 1} {:doll-value 2} {:doll-value 3}])
   (is (= 6 (total-value somevar)))
   )
+
+(deftest combo-weight-returns-total-weight
+  (def somevar [{:doll-weight 1} {:doll-weight 2} {:doll-weight 3}])
+  (is (= 6 (combo-weight somevar)))
+  )
