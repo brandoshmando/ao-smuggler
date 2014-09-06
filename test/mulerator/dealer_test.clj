0b00e1b70718098530730b9d3fee1026e2fd2f94
brandoshmando <brancraft@gmail.com>
2014-09-05 22:35:08 -0600

    Added test to check if to_i method does in fact return an integer

--- test/mulerator/dealer_test.clj
+++ test/mulerator/dealer_test.clj
@@ -1,6 +1,6 @@
 (ns mulerator.dealer-test
   (:require [clojure.test :refer :all]
-            [mulerator.dealer :refer [spacer]]))
+            [mulerator.dealer :refer [spacer to_i]]))
 
 ; Test for spacer somewhat pointless since it returns nil but does check to see tht identical function returns the same
 (deftest test-spacer-is-line
@@ -9,3 +9,8 @@
   (is (= (result) (spacer )))
   )
 
+(deftest to-i-returns-integer
+  (def somevar "200")
+  (is (= 200 (to_i somevar)))
+  (is (instance? Integer (to_i somevar)))
+  )
